5c329e43a0b295bc98901bf7b72a679a32381135
MINKI PAEK <pakmac@MINKIui-MacBookPro.local>
2025-07-27 20:00:49 +0900

    Fix Netlify build issues - Update dependencies and configuration

---
+++ backup/src/components/AdPlaceholder.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AdPlaceholder() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        minHeight: 90,
+        background: "#f3f3f3",
+        border: "1.5px dashed #bbb",
+        borderRadius: 8,
+        margin: "24px 0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#888",
+        fontSize: 16,
+      }}
+    >
+      광고 자리 (Ad Placeholder)
+    </div>
+  );
+}
+
+export default AdPlaceholder; 
